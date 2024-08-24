302a16171b8b449735dd494b27aa711bf027ebdd
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-24 17:13:24 +0530

    input.cpp

---
+++ input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the value of a:";
+    cin>>a;
+    
+    int b;
+    cout<<"Enter the value of b:";
+    cin>>b;
+    
+    cout<<"Value of a is:"<< a <<endl;
+    cout<<"Value of b is:"<< b <<endl;
+    
+    return 0;
+}
