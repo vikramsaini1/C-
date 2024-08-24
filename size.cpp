4e7241324d9f00a165a9c9338df7b5bb8fcc0b1f
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-24 17:29:22 +0530

    size.cpp

---
+++ size.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int intergertype;
+    char chartype;
+    double doubletype;
+    float floattype;
+    
+    
+    cout<<"Size of the int:"<< sizeof(intergertype)<<endl;
+     cout<<"Size of the char:"<< sizeof(chartype)<<endl;
+      cout<<"Size of the double:"<< sizeof(doubletype)<<endl;
+       cout<<"Size of the flaot:"<< sizeof(floattype)<<endl;
+    
+    return 0;
+}
