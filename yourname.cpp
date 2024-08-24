798845aa2e217d59641efbbf29c56dc8386326e2
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-24 17:05:17 +0530

    yourname.cpp

---
+++ yourname.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+  
+
+int main() 
+{ 
+//   int name; 
+// ham yeh int use nhi kr skte kyu ke int tho function value ko show krta h 
+string name;
+    //   string use krte h naam ke liyh 
+  cout << "Enter the name: "; 
+  cin >> name; 
+    
+  cout << "Entered name is: " <<  
+           name;  
+    return 0; 
+}
