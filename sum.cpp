203615b78a1bf4ff0037a1af3cb8d88e4a18a46e
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-24 17:16:22 +0530

    sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the value of a:";
+    cin>>a;
+    
+    int b;
+    cout<<"Enter the value of b:";
+    cin>>b;
+    
+    int sum;
+    sum= a+ b;
+    cout<<"Sum of a and b is:"<<sum<<endl;
+    
+    return 0;
+}
