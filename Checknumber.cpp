761a5d5df2976f46de022e3354cdc27b63d7795b
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-25 20:38:30 +0530

    Checknumber.cpp

---
+++ Checknumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cout<<"Enter the value of a:"<<endl;
+  cin>>a;
+  
+  if(a>0){
+      cout<<"a is postive number"<<endl;
+     }
+      else if(a<0){
+        cout<<"a is negative number"<<endl;
+    }
+  else{
+      cout<<"a is zero"<<endl;
+  }
+    return 0;
+    
+}
