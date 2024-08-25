e004f9f38dd1e86a4f8eb7584ed33bcef7854919
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-25 19:16:19 +0530

    simpleinterest.cpp

---
+++ simpleinterest.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int p;
+  cout<<"Enter the Value of Principle P"<<endl;
+  cin>>p;
+  
+  
+  int r;
+  cout<<"Enter the Value of rate r"<<endl;
+  cin>>r;
+  
+  int t;
+  cout<<"Enter the Value of time t"<<endl;
+  cin>>t;
+  
+ int simpleinterest;
+ simpleinterest=(p*r*t)/2;
+ 
+ cout<<"Simple Interest"<<simpleinterest<<endl;
+    
+    return 0;
+    
+}
