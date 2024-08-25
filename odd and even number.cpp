939e4051482d6a50731763f017d05bc6ee638163
vikramsaini1 <137759519+vikramsaini1@users.noreply.github.com>
2024-08-25 20:52:20 +0530

    check odd and even number.cpp

---
+++ odd and even number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cout<<"Enter the value of a:"<<endl;
+  cin>>a;
+  
+  
+  
+  if(a%2==0){
+      cout<<"a even number"<<endl;
+     }
+     
+  else{
+      cout<<"a is odd number"<<endl;
+  }
+    return 0;
+    
+}
